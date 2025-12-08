1a5b7ccd19ecefd6b5d849fb10700298d78af938
talhaa23 <talhasifat23@gmail.com>
2025-12-08 11:47:45 +0500

    feat: 08 Dec

--- landing-page.js
+++ landing-page.js
@@ -50,9 +50,6 @@ form.addEventListener('submit', async e => {
   data.append('phone', form.phone.value);
   data.append('unit_interest', form.unit_interest.value);
 
-  // data.append("hs_lead_status", form.querySelector('[name="unit_interest"]:checked').value);
-  // data.append("hs_lead_status", "open");
-
   const res = await fetch(
     "https://forms.hubspot.com/uploads/form/v2/39561244/fde1efd1-85eb-4fea-a65b-b0eb9f0b7b2c",
     { method: "POST", body: data }
